8c58ad0fafd5b2083788a04c1008ce9305863361
Renan <machadosantosrenan@gmail.com>
2025-11-29 16:41:15 -0300

    fix: Adjusted recipes conclusion times.

--- fase-1/constantes.h
+++ fase-1/constantes.h
@@ -28,6 +28,6 @@ const vector<vector<int>> INGREDIENTES = {
 };
 
 const int MAX_INGREDIENTES = 7;
-const int MIN_TEMPO = 20;
-const int MAX_TEMPO = 40;
+const int MIN_TEMPO = 30;
+const int MAX_TEMPO = 60;
 #endif
