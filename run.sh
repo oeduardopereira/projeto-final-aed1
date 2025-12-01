887c74c7e96c28c1351913187074f740f1467e83
Renan <machadosantosrenan@gmail.com>
2025-12-01 15:07:49 -0300

    Updated run.sh

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 if [ -n "$1" ]; then
 	if [["$1" != "--not-run"]]; then
-		g++ $1 -o app -lsfml-graphics -lsfml-window -lsfml-system -utf-8
+		g++ $1 -o app -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system -utf-8 -lFLAC
 		./app
 	else
-		g++ main.cpp -o app -lsfml-graphics -lsfml-window -lsfml-system -utf-8
+		g++ main.cpp -o app -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system -utf-8 -lFLAC
 		echo "Compiled with success"
 	fi
 else
-	g++ main.cpp -o app -lsfml-graphics -lsfml-window -lsfml-system -utf-8
+	g++ main.cpp -o app -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system -utf-8 -lFLAC
 	./app
 fi
 
