4dc23ccb0fb8c3466b873440a7b5a8891b6b5dbe
Renan <machadosantosrenan@gmail.com>
2025-12-01 20:41:55 -0300

    fix: Project Structure

--- compile-win.sh
+++ compile-win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-x86_64-w64-mingw32-g++ -O2 main.cpp -o $1 -I/home/renan/sfml_windows/SFML-2.6.2/include -L/home/renan/sfml_windows/SFML-2.6.2/lib -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system -mwindows
+x86_64-w64-mingw32-g++ -O2 main.cpp -o app.exe -I/home/renan/sfml_windows/SFML-2.6.2//include/ -L/home/renan/sfml_windows/SFML-2.6.2//lib -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system -mwindows
--- fase-2/README.md
+++
@@ -1,2 +0,0 @@
-# projeto-final-aed1 - fase 2
-Projeto final para a matéria de algoritmos e estruturas de dados 1
